20c6de20f6586a0973eb73f282a352747b2f86b2
Diego Castro <diegojcr4@gmail.com>
2021-08-30 16:08:38 -0400

    Added generateRandomString() function

--- express-server.js
+++ express-server.js
@@ -6,6 +6,28 @@ const PORT = 8080; // default port 8080
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+const generateRandomString = () => {
+  let output = [];
+  // We want it to always be 6 digits long, arbitrarily chosen.
+  for (let i = 0; i < 6; i++) {
+    // Generates new alphanumeric case-sensitive char
+    const newChar = Math.floor(Math.random() * 62);
+    let finalIndex;
+    if (newChar < 10) {
+      // 0-9 start at index 48
+      finalIndex = newChar + 48;
+    } else if (newChar < 36) {
+      // A-Z start at index 65, subtracting 11 gives 55
+      finalIndex = newChar + 55;
+    } else {
+      // a-z start at index 97, subtracting 36 gives 61
+      finalIndex = newChar + 61;
+    }
+    output.push(String.fromCharCode(finalIndex));
+  }
+  return output.join('');
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
