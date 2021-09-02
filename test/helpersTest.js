6e2fd69427ad2bd418280e09e4987d3c537d4d48
Diego Castro <diegojcr4@gmail.com>
2021-09-02 16:39:00 -0400

    add and lint helper test functions

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,14 +4,24 @@ const { getUserByEmail, generateRandomString, urlsForUserId } = require('../help
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
+  }
+};
+const testDatabase = {
+  testId1: {
+    longURL: "https://www.tsn.ca",
+    userID: "userRandomID"
+  },
+  testId2: {
+    longURL: "https://www.google.ca",
+    userID: "userRandomID"
   }
 };
 
@@ -25,12 +35,12 @@ describe('getUserByEmail', () => {
   it('should return a user object when given a valid email', () => {
     const user = getUserByEmail("user2@example.com", testUsers);
     const expectedOutput = {
-      id: "user2RandomID", 
-      email: "user2@example.com", 
+      id: "user2RandomID",
+      email: "user2@example.com",
       password: "dishwasher-funk"
     };
     assert.deepEqual(user, expectedOutput);
-  }); 
+  });
 
   it('should return undefined when given an email absent from the DB', () => {
     const user = getUserByEmail("badexample@wrongdomain.com", testUsers);
@@ -44,9 +54,32 @@ describe('generateRandomString', () => {
     const random = generateRandomString();
     assert.equal(random.length, 6);
   });
+
   it('should generate different strings every time', () => {
     const random1 = generateRandomString();
     const random2 = generateRandomString();
     assert.notEqual(random1, random2);
+  });
+});
+
+describe('urlsForUserId', () => {
+  it('should return an object with userID property matching argument', () => {
+    const matches = urlsForUserId('userRandomID', testDatabase);
+    const expected = {
+      testId1: {
+        longURL: "https://www.tsn.ca",
+        userID: "userRandomID"
+      },
+      testId2: {
+        longURL: "https://www.google.ca",
+        userID: "userRandomID"
+      }
+    };
+    assert.deepEqual(matches, expected);
+  });
+
+  it('should return an empty object if userID arguments has no matching URLs', () => {
+    const matches = urlsForUserId('user2RandomID', testDatabase);
+    assert.isEmpty(matches);
   });
 });
